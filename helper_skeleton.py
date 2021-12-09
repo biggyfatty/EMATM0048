7c65b26d72e004509089a6d06129da1a0ea1b133
biggyfatty <isadora.shi@gmail.com>
2021-12-09 20:51:17 +0000

    Update helper_skeleton.py

--- helper_skeleton.py
+++ helper_skeleton.py
@@ -4,7 +4,7 @@ class Board:
     def update_board(self,....):
         # use this function to update the board after move
 
-    # other methods needed for print board, get winners etc
+    # other methods needed for print board, annouce the winner etc
 
 class Player:
     def __init___(self,...):
