b619772ebc886edd9ca305b8ebf29c7e266e5c89
biggyfatty <isadora.shi@gmail.com>
2021-12-09 20:40:53 +0000

    add stuff to main skeleton

--- main_skeleton.py
+++ main_skeleton.py
@@ -2,3 +2,13 @@ from helper_skeleton import Board, Player
 
 
 def main():
+
+    # initialise two players
+    player1 = Player(...)
+    ......
+    # make move and update board
+
+    # end game when approriate
+
+# run main function
+
